cec6a313c9a9a21aadd0262fb45304a945dff5ef
anthonyatras <anthonyatras@gmail.com>
2024-07-10 09:04:32 -0400

    Subiendo primer commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function Enviar() {
+  Swal.fire({
+    title: "Do you want to save the changes?",
+    showDenyButton: true,
+    showCancelButton: true,
+    confirmButtonText: "Save",
+    denyButtonText: `Don't save`,
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire("Saved!", "", "success");
+    } else if (result.isDenied) {
+      Swal.fire("Changes are not saved", "", "info");
+    }
+  });
+}
