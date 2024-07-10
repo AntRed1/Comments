0232f9665aef9af08d8657fb88965f483e80ba8d
arojas <arojas@bsc.com.do>
2024-07-10 11:10:54 -0400

    Agregando Sweet Alert

--- js/main.js
+++ js/main.js
@@ -1,15 +1,32 @@
+function validarCampos(nombre, apellidos, email, comentarios) {
+  return nombre && apellidos && email && comentarios;
+}
+
 function Enviar() {
-  Swal.fire({
-    title: "Do you want to save the changes?",
-    showDenyButton: true,
-    showCancelButton: true,
-    confirmButtonText: "Save",
-    denyButtonText: `Don't save`,
-  }).then((result) => {
-    if (result.isConfirmed) {
-      Swal.fire("Saved!", "", "success");
-    } else if (result.isDenied) {
-      Swal.fire("Changes are not saved", "", "info");
-    }
-  });
+  var nombre = document.getElementById("nombre").value.trim();
+  var apellidos = document.getElementById("apellidos").value.trim();
+  var email = document.getElementById("email").value.trim();
+  var comentarios = document.getElementById("comentarios").value.trim();
+
+  if (validarCampos(nombre, apellidos, email, comentarios)) {
+    Swal.fire({
+      icon: "error",
+      title: "Existen Campos vacios",
+      text: "Por favor, complete todos los campos.",
+    });
+  } else {
+    Swal.fire({
+      title: "Desea guardar los cambios?",
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: "Guardar",
+      denyButtonText: `No Guardar`,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire("Guardado!", "", "success");
+      } else if (result.isDenied) {
+        Swal.fire("Los cambios no fueron guardados", "", "info");
+      }
+    });
+  }
 }
